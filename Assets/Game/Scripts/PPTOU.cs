24f3763c9d3a76059a06256ad66ab4cff53ea6b3
omermesebuken1 <76281192+omermesebuken1@users.noreply.github.com>
2023-04-20 14:32:11 +0300

    sondu galiba pp tou

---
+++ Assets/Game/Scripts/PPTOU.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PPTOU : MonoBehaviour
+{
+    
+    public void PP()
+    {
+        Application.OpenURL("https://doc-hosting.flycricket.io/soft-blast-3d-privacy-policy/5291f3b3-7775-4e0a-b4ad-1fd78139209f/privacy");
+    }
+
+    public void TOU()
+    {
+        Application.OpenURL("https://doc-hosting.flycricket.io/soft-blast-3d-terms-of-use/f400b756-5458-445a-9de2-27ff898c7729/terms");
+    }
+
+}
